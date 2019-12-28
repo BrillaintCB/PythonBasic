516d49e67b723994e658b3c1d01d86d69d095828
parkaet@gmail.com <parkaet@gmail.com>
2019-12-28 18:05:50 +0900

    practice

--- TheFirstPython/TheFirstPython/D1/PM/prac4.py
+++ TheFirstPython/TheFirstPython/D1/PM/prac4.py
@@ -14,5 +14,24 @@ if (castedint is castedfloat):
 else:
     print("Different")
     
-print(123+123)
+print("abc\ta\tabcdefg\tabcdefgh\t")
 print(123 + 123)
+
+
+
+nums = [12,54,268,42,2,23,120,3]
+
+result = "{0}+{1}={2} 입니다\n{3}-{4}={5} 입니다\n{6}*{7}={8} 입니다\n{9}/{10} = {11} 입니다"
+
+print(result.format(nums[0],nums[1],nums[0]+nums[1],
+                    nums[2],nums[3],nums[2]-nums[3],
+                    nums[4],nums[5],nums[4]*nums[5],
+                    nums[6],nums[7],nums[6]/nums[7]))
+
+
+
+
+
+
+
+
