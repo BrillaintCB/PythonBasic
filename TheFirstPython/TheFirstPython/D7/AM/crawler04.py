c40d9e7d6d92ee7d90fa0d2a352e75e88b5b4a78
parkaet@gmail.com <parkaet@gmail.com>
2020-02-15 16:35:53 +0900

    wd

--- TheFirstPython/TheFirstPython/D7/AM/crawler04.py
+++ TheFirstPython/TheFirstPython/D7/AM/crawler04.py
@@ -5,9 +5,7 @@ html = urlopen('http://www.hanjin.co.kr/Delivery_html/inquiry/result_waybill.jsp
 bsObject = BeautifulSoup(html, "html.parser")
 
 tracking = []
-
 detail = []
-#print(bsObject)
 
 for t in bsObject.find_all('div', {'id':'result_waybill2'}):
     #tracking.append(str(t.select('tbody')[1].select('tr')))
@@ -16,12 +14,45 @@ for t in bsObject.find_all('div', {'id':'result_waybill2'}):
     tracking.append(t.select('tbody')[1].select('td'))
 
 
+names = []
+date = []
+time = []
+stat = []
 
 
 #print(tracking)
 
+for index, stat in enumerate(tracking):
+    #print(index, str(stat).replace('<td>','').replace('</td>','').splitlines())
+    names.append(str(stat).replace('<td>','').replace('</td>','').splitlines())
+    
+    
+
+#print(str(names[0][0]).split(','))
+#for index, content in enumerate(names):
+    #date.append(str(str(names[0][0]).split(',')[0]).strip('['))
+    #time.append(str(names[0][0]).split(',')[1])
+    #stat.append(str(names[0][0]).split(',')[2])
+#print(date)
 
 
 
+#print("Date" ,str(str(names[0][0]).split(',')[0]).strip('['))
+#print("Time" ,str(names[0][0]).split(',')[1])
+#print("Name" ,str(names[0][0]).split(',')[2])
+
+#print("Name" ,str(names[0][1]).split(',')[3])
 
 
+#print(str(names[0]).split('<div align="left">')[0])
+#print(str(names[0]).split('<div align="left">')[1])
+#print(str(names[0]).split('<div align="left">')[2])
+#print(str(names[0]).split('<div align="left">')[3])
+
+
+
+print(str(str(str(names[0]).split('<br/>')[0]).split(',')[0]).lstrip("\['["))
+print(str(str(str(names[0]).split('<br/>')[0]).split(',')[1]).strip(' '))
+print(str(str(str(names[0]).split('<br/>')[0]).split(',')[2]).strip(' '))
+print(str(str(str(names[0]).split('<br/>')[0]).split(',')[4]).strip(' ').lstrip("'<div align=\"left\">'"))
+    
